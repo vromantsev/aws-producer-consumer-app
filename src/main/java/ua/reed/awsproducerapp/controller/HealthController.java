47cbcd79a3e8868d33ad6d2d0d5b04e07e66111e
vromantsev <vladyslav.romantsev@gmail.com>
2024-05-13 00:43:51 +0300

    added health check service

---
+++ src/main/java/ua/reed/awsproducerapp/controller/HealthController.java
@@ -0,0 +1,16 @@
+package ua.reed.awsproducerapp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("Service is up and running");
+    }
+}
